e7b8bfbefbc5c80d2e347c90e9713c9fcd68a262
Dominik Moreira <dominik.privatacc@gmail.com>
2026-05-04 22:14:49 +0200

    Initial Commit

---
+++ AnimationSandbox/AnimationSandboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationSandboxApp.swift
+//  AnimationSandbox
+//
+//  Created by Dominik de Jesus Moreira on 04.05.26.
+//
+
+import SwiftUI
+
+@main
+struct AnimationSandboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationSandbox/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AnimationSandbox
+//
+//  Created by Dominik de Jesus Moreira on 04.05.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
